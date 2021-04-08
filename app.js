cc838ffa666c5e98b04a65e1d671a78e7964e282
illonion <zhou.lawrence.yz@gmail.com>
2021-04-09 00:00:58 +1000

    Complete Landing Page and Initialise Rules Page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({
 app.get("/", function(req, res) {
     res.render("index");
 })
+
+app.get("/rules", function(req, res) {
+    res.render("rules");
+})
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
